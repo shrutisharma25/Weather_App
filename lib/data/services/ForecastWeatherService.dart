7a64289538d949b294a41682a20b2bb9ee4585c7
Shruti Sharma <shruti1005sharma@gmail.com>
2024-03-14 15:13:07 +0530

    commit

--- lib/data/services/ForecastWeatherService.dart
+++ lib/data/services/ForecastWeatherService.dart
@@ -5,8 +5,8 @@ import 'package:weather_app/data/models/Forecast.dart';
 import 'package:weather_app/utils/consts.dart';
 
 class ForecastWeatherService {
-  Future<List<Forecast>?> fetchForecastWeather(
-      String latitude, String longitude) async {
+  Future<List<Forecast>?> fetchHourlyWeather(
+      String latitude, String longitude, String todaysDate) async {
     final fullUrl =
         Weather_URL + 'forecast?location=$latitude,$longitude&apikey=$API_Key';
 
@@ -26,10 +26,8 @@ class ForecastWeatherService {
         print("temperature-->" + temperature.toString());
 
         DateTime dateTime = DateTime.parse(time);
-        String todaysDate = DateFormat('yyyy-MM-dd').format(dateTime);
         int hour = dateTime.hour;
 
-// Check if the hour is a multiple of 3
         if (hour % 3 == 0) {
           print("todaysDate-->" + todaysDate.toString());
           print("time-->" + time);
@@ -42,8 +40,43 @@ class ForecastWeatherService {
             ));
           }
         }
+      }
+      return forecasts;
+    } else if (response.statusCode == 429) {
+      final jsonResponse = json.decode(response.body);
+      final errorMessage = jsonResponse['message'];
+      forecasts.add(Forecast(time: "", temperature: "", error: errorMessage));
+      return forecasts;
+    } else {
+      return null;
+    }
+  }
 
+  Future<List<Forecast>?> fetchDailyWeather(
+      String latitude, String longitude) async {
+    final fullUrl =
+        Weather_URL + 'forecast?location=$latitude,$longitude&apikey=$API_Key';
 
+    print("fullUrlForeCast-->" + fullUrl);
+    final response = await http.get(Uri.parse(fullUrl));
+    print("response2-->" + response.toString());
+
+    List<Forecast> forecasts = [];
+    if (response.statusCode == 200) {
+      final jsonResponse = json.decode(response.body);
+      print("jsonResponse-->" + jsonResponse.toString());
+      final data = jsonResponse['timelines']['daily'];
+      print("data2-->" + data.toString());
+      for (var item in data) {
+        final time = item['time'].toString();
+        String temperature = item['values']['temperatureAvg'].toString();
+        print("temperature-->" + temperature.toString());
+        print("timefore-->" + time);
+
+        forecasts.add(Forecast(
+          time: time,
+          temperature: temperature,
+        ));
       }
       return forecasts;
     } else if (response.statusCode == 429) {
--- lib/screens/ForecastWeatherScreen/ForecastWeatherScreen.dart
+++ lib/screens/ForecastWeatherScreen/ForecastWeatherScreen.dart
@@ -18,48 +18,164 @@ class ForecastWeatherScreen extends StatefulWidget {
 class _ForecastWeatherScreenState extends State<ForecastWeatherScreen> {
   final ForecastWeatherService forecastService = ForecastWeatherService();
   List<Forecast>? forecastData;
+  List<Forecast>? forecastDailyData;
+  bool isLoading = true;
+  String todaysDate =
+  DateFormat('yyyy-MM-dd', 'en_US').format(DateTime.now().toUtc());
 
   @override
   void initState() {
     super.initState();
-    _fetchForecastData();
+    _fetchHourlyData();
   }
 
-  Future<void> _fetchForecastData() async {
-    final fetchedData = await forecastService.fetchForecastWeather(widget.latitude, widget.longitude);
-    print("forecastData-->"+fetchedData.toString());
-    setState(() {
-      forecastData = fetchedData;
-    });
+  Future<void> _fetchHourlyData() async {
+    try {
+      final fetchedData = await forecastService.fetchHourlyWeather(
+          widget.latitude, widget.longitude, todaysDate);
+      final fetchedDailyData = await forecastService.fetchDailyWeather(
+          widget.latitude, widget.longitude);
+      print("fetchedData-->" + fetchedData.toString());
+      print("fetchedDailyData-->" + fetchedDailyData.toString());
+      setState(() {
+        forecastData = fetchedData;
+        forecastDailyData = fetchedDailyData;
+        isLoading = false;
+      });
+    } catch (error) {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  String getWeatherImage(double temperature) {
+    if (temperature > 25) {
+      return "assets/images/hot.png"; // Image file name for hot weather
+    } else {
+      return "assets/images/cold.png"; // Image file name for cold weather
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return forecastData != null
-        ? ListView.builder(
-      itemCount: forecastData!.length,
-      itemBuilder: (context, index) {
-        Forecast forecast = forecastData![index];
-        return Card(
-          clipBehavior: Clip.hardEdge,
-          child: InkWell(
-            splashColor: Colors.blue.withAlpha(30),
-            child: SizedBox(
-              width: 300,
-              height: 100,
-              child: Row(
-                children: [
-                  Text('Time: ${DateFormat.jm().format(DateTime.parse(forecast.time))}'),
-                  Text('Temperature: ${forecast.temperature}°C')
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    )
-        : Center(
-      child: CircularProgressIndicator(),
+    return Column(
+      children: [
+        ForeCastHourly(),
+        SizedBox(height: 10,),
+        ForeCastDaily()
+      ],
     );
   }
+
+  Widget ForeCastHourly(){
+    return isLoading
+        ? Center(
+      child: CircularProgressIndicator(),
+    )
+        : forecastData != null
+        ? Container(
+      height: 90,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: forecastData!.length,
+        itemBuilder: (BuildContext context, int index) {
+          final forecast = forecastData![index];
+          final weatherImage =
+          getWeatherImage(double.parse(forecast.temperature));
+          return Padding(
+            padding: const EdgeInsets.all(8.0), // Adjust padding as needed
+            child: Card(
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                child: SizedBox(
+                  width: 100,
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // Image.asset(
+                          //   weatherImage,
+                          //   width: 10, // Adjust width as needed
+                          //   height: 5, // Adjust height as needed
+                          // ),
+                          // SizedBox(height: 10,),
+                          Text(
+                              '${DateFormat.jm().format(DateTime.parse(forecast.time))}'),
+                          Text('${forecast.temperature}°C')
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    )
+        : Center(
+      child: Text('No forecast data available.'),
+    );
+  }
+
+  Widget ForeCastDaily(){
+    return isLoading
+        ? Center(
+      child: CircularProgressIndicator(),
+    )
+        : forecastData != null
+        ? Container(
+      child: Card(
+        clipBehavior: Clip.hardEdge,
+        child: SizedBox(
+          width: 350,
+          height: 300,
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: forecastData!.length,
+            itemBuilder: (BuildContext context, int index) {
+              final forecast = forecastData![index];
+              final weatherImage =
+              getWeatherImage(double.parse(forecast.temperature));
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(6, 0, 6, 5), // Adjust padding as needed
+                child: Card(
+                  clipBehavior: Clip.hardEdge,
+                  child: InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                    child: SizedBox(
+                      width: 90,
+                      height: 40,
+                      child: Row(
+                        children: [
+                          // Image.asset(
+                          //   weatherImage,
+                          //   width: 10, // Adjust width as needed
+                          //   height: 5, // Adjust height as needed
+                          // ),
+                          // SizedBox(height: 10,),
+                          Text(
+                              '${DateFormat.jm().format(DateTime.parse(forecast.time))}'),
+                          Text('${forecast.temperature}°C')
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        )
+      )
+    )
+        : Center(
+      child: Text('No forecast data available.'),
+    );
+  }
+
 }
